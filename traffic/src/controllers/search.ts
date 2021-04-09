464ea45bbbfc98bee0922403c9f89bdb124b8360
Camilo Gonzalez <cgs.camilogonzalez@gmail.com>
2021-04-08 23:54:32 -0400

    read file stream and pass through to aws s3 bucket

--- traffic/src/controllers/search.ts
+++ traffic/src/controllers/search.ts
@@ -1,7 +1,33 @@
 import { Router } from 'express';
+import Busboy from 'busboy';
+import AWS from 'aws-sdk';
+import { PassThrough } from 'stream';
 
 const router = Router();
+const s3 = new AWS.S3({ region: 'us-east-1' });
 
-router.get('/', (_, res) => res.json({ message: 'Search endpoint' }));
+router.post('/', (req, res) => {
+  const busboy = new Busboy({ headers: req.headers });
+
+  busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
+    file.pipe(uploadFromStream(s3, filename));
+
+    file.on('end', () => {
+      res.status(204).send();
+    });
+  });
+
+  req.pipe(busboy);
+});
+
+function uploadFromStream(s3: AWS.S3, filename: string) {
+  const pass = new PassThrough();
+
+  s3.upload({ Bucket: 'news-now-s3', Key: filename, Body: pass }, (err) => {
+    console.error(err);
+  });
+
+  return pass;
+}
 
 export default router;
