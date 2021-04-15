01b1e0004fea02d587fa0e03ade7e0002053c7ac
kjy1023 <kjy1023@g.rit.edu>
2021-04-15 19:09:46 -0400

    finally got mic recording to work

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,5 +1,73 @@
 import React from 'react';
+import { Pane, Heading, SearchInput } from 'evergreen-ui';
+import Container from '../components/container';
+import Header from '../components/header';
+const MicRecorder = require('mic-recorder-to-mp3');
+ 
+const recorder = new MicRecorder({
+    bitRate: 128
+});
 
-const HomePage: React.FC = () => <h1>News Now!</h1>;
+function startRecord(recorder) {
+    let win
+    try {
+        console.log("something else")
+        win = window
+        recorder.start().then(() => {
+
+        }).catch((e) => {
+            console.error(e);
+        });
+
+    } catch (error) {
+        win = {} 
+        console.log("could not find window");
+    } 
+}
+
+function stopRecord(recorder) {
+    console.log("something")
+    try {
+        console.log("something")
+        recorder.stop().getMp3().then(([buffer, blob]) => {
+            // do what ever you want with buffer and blob
+            // Example: Create a mp3 file and play
+            const file = new File(buffer, 'me-at-thevoice.mp3', {
+            type: blob.type,
+            lastModified: Date.now()
+
+            
+        });
+        const player = new Audio(URL.createObjectURL(file));
+        player.play();
+
+        }).catch((e) => {
+            alert('We could not retrieve your message');
+            console.log(e);
+        });
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const HomePage: React.FC = () => ( 
+    <Pane
+    width="100%"
+    minHeight="100vh"
+    maxHeight="100%"
+    paddingY={20}
+    background="blueTint"
+    >
+        <Container>
+            <Header />
+        </Container>
+        <Container>
+            <div>
+                <button onClick={() => startRecord(recorder)}>record</button>
+                <button onClick={() => stopRecord(recorder)}>play</button>
+            </div>
+        </Container>
+    </Pane>
+);
 
 export default HomePage;
