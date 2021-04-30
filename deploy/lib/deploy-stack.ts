67cad14d758f8d76efa3b953aed0ee3164cb7417
Camilo Gonzalez <cgs.camilogonzalez@gmail.com>
2021-04-30 00:52:16 -0400

    add deploy news now bucket and website bucket

--- deploy/lib/deploy-stack.ts
+++ deploy/lib/deploy-stack.ts
@@ -1,9 +1,28 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import * as s3 from "@aws-cdk/aws-s3";
+import * as iam from "@aws-cdk/aws-iam";
 
 export class DeployStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const webBucket = new s3.Bucket(this, "NewsNowWebBucket", {
+      publicReadAccess: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      websiteIndexDocument: "index.html",
+    });
+
+    const newsNowBucket = new s3.Bucket(this, "NewsNowBucket", {
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    const newsNowBucketPolicy = new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      actions: ["s3:*"],
+      resources: [`${newsNowBucket.bucketArn}/*`],
+      principals: [new iam.AnyPrincipal()],
+    });
+
+    newsNowBucket.addToResourcePolicy(newsNowBucketPolicy);
   }
 }
